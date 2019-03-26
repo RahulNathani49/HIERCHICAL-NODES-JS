d1960e0a9798730d35af82f7ad6a82b3e685f6d9
RahulNathani49 <nathanirahul49@gmail.com>
2019-03-26 19:48:33 +0530

    nodes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-function createnode(x){
-    var creationul = document.createElement("ul");
-    x.parentNode.appendChild(creationul); creationul.innerHTML= '<li contenteditable="true" id="newchild"  >NAME - VALUE<button  onclick="createnode(this)" contenteditable="false">+</button></li>';  
+function createnode(x) {
+    console.log(x.parentElement.lastElementChild);
+    var creationul = x.parentElement.lastElementChild;
+    var creationli = document.createElement("li");
+    x.parentNode.appendChild(creationul);
+    creationul.appendChild(creationli);
+    creationli.innerHTML = 'NAME - VALUE<button  onclick="createnode(this)" contenteditable="false">+</button><ul></ul>';
 }
-
-
-
-
